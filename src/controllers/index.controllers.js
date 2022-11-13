3daeaff2724dcfc39e5fda34e4320beb5da39c02
CharlesVP15 <youquince@gmail.com>
2022-11-13 03:02:39 -0500

    Add configuration database connect and project

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,7 +1,8 @@
 const { v4: uuidv4 } = require('uuid')
 const { client } = require('../database')
+const { admin } = require('../firebase/serviceCredentials')
 const {
-   schemaStudents
+   schemaStudents, schemaSignup
 } = require('../validations/schemasValidators')
 
 const idxCtrl = {}
@@ -10,6 +11,40 @@ idxCtrl.welcomePage = (req, res) => {
    return res.status(200).json({
       msg: 'Welcome to API Challenge'
    })
+}
+
+idxCtrl.signupUser = async (req, res) => {
+   try {
+      const { email, password } = req.body
+
+      if ( !email || !password ) {
+         return res.status(400).json({
+            msg: 'Todos los campos son requeridos'
+         })
+      }
+
+      const emailN = email.trim(),
+         passwordN = password.trim()
+
+      const msg = schemaSignup(emailN, passwordN)
+      if (msg) return res.status(400).json({ msg })
+
+      const resp = await admin.auth().createUser({
+         email: emailN,
+         password: passwordN
+      })
+
+      return res.status(200).json({
+         msg: 'Autenticación exitosa',
+         data: resp
+      })
+   } catch (e) {
+      console.error(e)
+
+      return res.status(500).json({
+         msg: 'Error del server. Intentelo más luego x_x.'
+      })
+   }
 }
 
 idxCtrl.saveStudents = async (req, res) => {
@@ -77,9 +112,17 @@ idxCtrl.saveSchedule = async (req, res) => {
          })
       }
 
-      const searchStd = await client.query('SELECT idStd FROM schedule WHERE idStd = ?', idStd)
+      const searchStd = await client.query('SELECT * FROM students WHERE id = ?', idStd)
 
-      if (searchStd.length > 0) {
+      if (searchStd.length === 0) {
+         return res.status(400).json({
+            msg: 'ID incorrecto del estudiante'
+         })
+      }
+
+      const searchSche = await client.query('SELECT idStd FROM schedule WHERE idStd = ?', idStd)
+
+      if (searchSche.length > 0) {
          return res.status(400).json({
             msg: 'El estudiante ya tiene su horario asignado'
          })
@@ -138,31 +181,19 @@ idxCtrl.searchSchedule = async (req, res) => {
       searchStd.forEach((item) => {
          const { desiredCourse, requiredCourse } = item
 
-         if (table.includes(requiredCourse)) {
-            if (table.includes(desiredCourse)) {
-               table.splice(table.indexOf(desiredCourse), 1)
-               table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
-            } else {
-               table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
-            }
-         } else {
-            if (table.includes(desiredCourse)) {
-               table.unshift(requiredCourse)
-               table.splice(table.indexOf(desiredCourse), 1)
-               table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
-            } else {
-               table.unshift(requiredCourse)
-               table.splice(table.indexOf(requiredCourse), 0, desiredCourse)
-            }
+         if (!table.includes(requiredCourse)) table = [requiredCourse, ...table]
+
+         if (table.includes(desiredCourse)) {
+            table.splice(table.indexOf(desiredCourse), 1)
          }
+
+         table.splice(table.indexOf(requiredCourse) + 1, 0, desiredCourse)
       })
 
       return res.status(200).json({
          msg: 'El estudiante tiene el horario asignado',
          data: table.map((item, idx) => { return {id: idx, asig: item} })
       })
-
-
    } catch (e) {
       console.error(e)
 
--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const client = mysql.createPool({
    host: process.env.DB_HOST,
    port: process.env.DB_PORT,
-   database: process.env.DB_NAME,
+   database: process.env.NODE_ENV === 'test' ? 'testdb' : process.env.DB_NAME,
    user: process.env.DB_USER,
    password: process.env.DB_PASSWORD,
 })
